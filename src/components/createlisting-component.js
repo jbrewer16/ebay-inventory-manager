150f5c8b8df0f57acde704d4df17c8839e8fd9d4
JT Brewer <badatnames1637@gmail.com>
2022-11-18 19:17:43 -0500

    Added create listing functionality

--- src/components/createlisting-component.js
+++ src/components/createlisting-component.js
@@ -1,10 +1,215 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 export default class CreateListing extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onChangeItemNumber = this.onChangeItemNumber.bind(this);
+    this.onChangeAmount = this.onChangeAmount.bind(this);
+    this.onChangeFinalProfit = this.onChangeFinalProfit.bind(this);
+    this.onChangeListingkLink = this.onChangeListingkLink.bind(this);
+    this.onChangeDateAdded = this.onChangeDateAdded.bind(this);
+    this.onChangeDateSold = this.onChangeDateSold.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      username: '',
+      itemnumber: 0,
+      amount: 0,
+      finalprofit: 0,
+      listinglink: "",
+      dateadded: new Date(),
+      datesold: new Date(),
+      users: [],
+      items: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/users/')
+      .then(response => {
+        if (response.data.length > 0) {
+          this.setState({
+            users: response.data.map(user => user.username),
+            username: response.data[0].username
+          });
+        }
+      });
+    axios.get('http://localhost:5000/items/')
+      .then(response => {
+        if (response.data.length > 0) {
+          this.setState({
+            items: response.data.map(item => item.itemnumber + ' - ' + item.itemname),
+            itemnumber: response.data[0].itemnumber
+          });
+        }
+      });
+  }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value
+    });
+  }
+
+  onChangeItemNumber(e) {
+    this.setState({
+      itemnumber: e.target.value
+    });
+  }
+
+  onChangeAmount(e) {
+    this.setState({
+      amount: e.target.value
+    });
+  }
+
+  onChangeFinalProfit(e) {
+    this.setState({
+      finalprofit: e.target.value
+    });
+  }
+
+  onChangeListingkLink(e) {
+    this.setState({
+      listinglink: e.target.value
+    });
+  }
+
+  onChangeDateAdded(d) {
+    this.setState({
+      dateadded: d
+    });
+  }
+
+  onChangeDateSold(d) {
+    this.setState({
+      datesold: d
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const listing = {
+      username: this.state.username,
+      itemnumber: this.state.itemnumber,
+      amount: this.state.amount,
+      finalprofit: this.state.finalprofit,
+      listinglink: this.state.listinglink,
+      dateadded: this.state.dateadded,
+      datesold: this.state.datesold
+    }
+
+    console.log(listing);
+
+    axios.post('http://localhost:5000/listings/add', listing)
+      .then(res => console.log(res.data))
+      .catch(err => console.log('Error: ' + err));
+
+    window.location = '/';
+
+  }
+
   render() {
     return (
       <div>
-        <p>You've reached the Create Listing component!</p>
+        <h3>Create New Listing</h3>
+        <form onSubmit={this.onSubmit}>
+          {/* Username */}
+          <div className='form-group'>
+            <label>Username: </label>
+            <select ref='userInput'
+              required
+              className='form-control'
+              value={this.state.username}
+              onChange={this.onChangeUsername}>
+              {
+                this.state.users.map(function (user) {
+                  return <option
+                    key={user}
+                    value={user}>{user}
+                  </option>
+                })
+              }
+            </select>
+          </div>
+          {/* Item Number */}
+          <div className='form-group'>
+            <label>Item Number: </label>
+            <select ref='userInput'
+              required
+              className='form-control'
+              value={this.state.itemnumber}
+              onChange={this.onChangeItemNumber}>
+              {
+                this.state.items.map(function (item) {
+                  return <option
+                    key={item}
+                    value={item}>{item}
+                  </option>
+                })
+              }
+            </select>
+          </div>
+          {/* Amount */}
+          <div className='form-group'>
+            <label>Amount: </label>
+            <input type='text'
+              required
+              className='form-control'
+              value={this.state.amount}
+              onChange={this.onChangeAmount}
+            />
+          </div>
+          {/* Final Profit */}
+          <div className='form-group'>
+            <label>Final Profit: </label>
+            <input type='text'
+              required
+              className='form-control'
+              value={this.state.finalprofit}
+              onChange={this.onChangeFinalProfit}
+            />
+          </div>
+          {/* Listing Link */}
+          <div className='form-group'>
+            <label>Listing Link: </label>
+            <input type='text'
+              required
+              className='form-control'
+              value={this.state.listinglink}
+              onChange={this.onChangeListingkLink}
+            />
+          </div>
+          {/* Date Added */}
+          <div className='form-group'>
+            <label>Date Added: </label>
+            <div>
+              <DatePicker
+                selected={this.state.dateadded}
+                onChange={this.onChangeDateAdded}
+              />
+            </div>
+          </div>
+          {/* Date Sold */}
+          <div className='form-group'>
+            <label>Date Sold: </label>
+            <div>
+              <DatePicker
+                selected={this.state.datesold}
+                onChange={this.onChangeDateSold}
+              />
+            </div>
+          </div>
+          <div className='form-group'>
+            <input type='submit' value='Create Listing' className='btn btn-primary' />
+          </div>
+        </form>
       </div>
     )
   }
